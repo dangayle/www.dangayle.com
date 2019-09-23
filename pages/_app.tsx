1d1acf1ed8860c4cd5ff07576ab331c0d9a313c3
Dan Gayle <dan.gayle@helloalpha.com>
2019-09-23 13:03:14 -0700

    Added google analytics via gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import App from 'next/app'
+import TagManager from 'react-gtm-module'
+
+
+const tagManagerArgs:object = {
+  gtmId: 'GTM-TPL9Q6V'
+}
+
 
 class MyApp extends App {
+  componentDidMount () {
+    TagManager.initialize(tagManagerArgs)
+  }
+
   render() {
     const { Component, pageProps } = this.props
     return <>
