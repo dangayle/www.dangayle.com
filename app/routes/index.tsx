647616b928f069dcc7a537a5af159b85162a935b
Dan Gayle <dangayle@gmail.com>
2023-02-25 12:04:08 -0800

    Updated to React 18 and latest Remix

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,21 @@
-
 import type { TextLinkProps } from "~/components/TextLink";
 import TextLink from "~/components/TextLink";
-import BaseTemplate from "~/layouts/BaseTemplate";
+import TwoColumn from "~/layouts/TwoColumn";
 import type { LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import data from "~/data/index.json";
-import type { MetaFunction } from "@remix-run/react/routeModules";
+import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction } from "@remix-run/node"; // or cloudflare/deno
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "preload",
+      href: "https://dangayle.mo.cloudinary.net/profile/IMG_2353.jpg?tx=ar_1:1,c_fill,g_auto:subject,h_600,w_600",
+      as: "image",
+    },
+  ];
+};
 
 export const loader: LoaderFunction = async () => {
   return data;
@@ -13,27 +23,33 @@ export const loader: LoaderFunction = async () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Dan Gayle",
+  title: "Daniel Gayle",
   description: "Full stack developer and #webperf engineer",
 });
 
 export default function Index() {
-  const {data:navList} = useLoaderData() || [];
+  const { data: navList } = useLoaderData() || [];
   return (
-    <BaseTemplate>
-      <nav className="text-xl">
-        <ul className="mb-8">
-          {navList.map((link:TextLinkProps) => (
-            <li key={link.href} className="mb-4 md:mb-2">
-              <TextLink text={link.text} href={link.href} />
-            </li>
-          ))}
-        </ul>
-        <h2 className="font-serif mb-4 text-2xl">Contact</h2>
-        <p>
-          <TextLink text="Email" href="mailto:dangayle@gmail.com" />
-        </p>
-      </nav>
-    </BaseTemplate>
+    <TwoColumn
+      navigation={
+        <>        
+          <img className="w-full mb-4" src="https://dangayle.mo.cloudinary.net/profile/IMG_2353.jpg?tx=ar_1:1,c_fill,g_auto:subject,h_600,w_600" alt="Daniel Gayle" />
+          <p>
+            <TextLink text="Email me" href="mailto:dangayle@gmail.com" />
+          </p>
+        </>
+      }
+      body={
+        <>
+          <ul className="mb-8">
+            {navList.map((link: TextLinkProps) => (
+              <li key={link.href} className="mb-4 md:mb-2">
+                <TextLink text={link.text} href={link.href} />
+              </li>
+            ))}
+          </ul>
+        </>
+      }
+    ></TwoColumn>
   );
 }
